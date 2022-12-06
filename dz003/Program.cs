b16fb5c4d964eb927577125785d59929447f6410
ipolenov <polenovilya71396@gmail.com>
2022-12-06 23:32:19 +0300

    add dz003

---
+++ dz003/Program.cs
@@ -0,0 +1,87 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.WriteLine("Enter matriz size m x n");
+Console.Write("Enter m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] matrix = new double [m,n];
+for(int i = 0; i < matrix.GetLength(0); i++)
+{
+    for(int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = Math.Round((Convert.ToDouble(new Random().Next(-10,10)) + new Random().NextDouble()),2);
+        Console.Write(matrix[i,j] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+//  и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+Console.WriteLine("Enter matriz size m x n");
+Console.Write("Enter m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int [m,n];
+for(int i = 0; i < matrix.GetLength(0); i++)
+{
+    for(int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = new Random().Next(0,11);
+        Console.Write(matrix[i,j] + " ");
+    }
+    Console.WriteLine();
+}
+if(x < m && y < n){Console.WriteLine(matrix[x,y]);}
+else{Console.WriteLine("Matrix dont have your number");}
+
+
+// Задача 52. Задайте двумерный массив из целых чисел.
+//  Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.WriteLine("Enter matriz size m x n");
+Console.Write("Enter m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int [m,n];
+for(int i = 0; i < matrix.GetLength(0); i++)
+{
+    for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0,11);
+            Console.Write(matrix[i,j] + " ");
+        }
+        Console.WriteLine();
+}
+Console.WriteLine();
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        sum += matrix[i, j];
+    }
+    Console.Write(Math.Round(sum / matrix.GetLength(0), 1) + " ");
+}
