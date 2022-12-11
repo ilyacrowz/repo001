d1908010abf105d18c2cbbe6379714a74fb53109
ipolenov <polenovilya71396@gmail.com>
2022-12-11 22:07:58 +0300

    dz004

--- dz002/Program.cs
+++ dz002/Program.cs
@@ -125,3 +125,96 @@ for (int i = 0; i < array.Length; i++)
 double result = max - min;
 Console.WriteLine(string.Join("; ", array));
 Console.WriteLine($"Max num - Min num = {Math.Round(result,2)}");
+
+//  //Задача 53: Задайте двумерный массив.
+// // Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+// int[,] FillMatrix(int rows, int columns)
+// {
+// int[,] matrix = new int [rows, columns];
+// for(int i = 0; i < matrix.GetLength(0); i++)
+// {
+// for(int j = 0; j < matrix.GetLength(1); j++)
+// {
+// matrix[i,j] = new Random().Next(0,10);
+// }
+// }
+// return matrix;
+// }
+
+// void PrintMatrix(int[,] matrix)
+// {
+// for(int i = 0; i < matrix.GetLength(0); i++)
+// {
+// for(int j = 0; j < matrix.GetLength(1); j++)
+// {
+// Console.Write(matrix[i,j] + " ");
+// }
+// Console.WriteLine();
+// }
+// }
+// int m = Convert.ToInt32(Console.ReadLine());
+// int n = Convert.ToInt32(Console.ReadLine());
+// int [,] matrix = FillMatrix(m,n);
+// PrintMatrix(matrix);
+// Console.WriteLine();
+
+// for(int j = 0; j < matrix.GetLength(1); j++)
+// {
+// int temp = matrix[0, j];
+// matrix[0,j] = matrix[matrix.GetLength(0)-1,j];
+// matrix[matrix.GetLength(0)-1,j] = temp;
+// }
+
+// PrintMatrix(matrix);
+
+//======================================================================================================
+
+// int m = Convert.ToInt32(Console.ReadLine());
+// int n = Convert.ToInt32(Console.ReadLine());
+// int [,] matrix = FillMatrix(m,n);
+// PrintMatrix(matrix);
+// Console.WriteLine();
+
+// for(int k = 0; k < 10; k++)
+// {
+// int count = 0;
+// for(int i = 0; i < matrix.GetLength(0); i++)
+// {
+// for(int j = 0; j < matrix.GetLength(1); j++)
+// {
+// if(matrix[i,j] == k)
+// {
+// count++;
+// }
+// }
+// }
+// if(count != 0) Console.WriteLine($"Кол-во повторений для {k} = {count}");
+// }
+
+//====================================================================================================
+
+// int m = Convert.ToInt32(Console.ReadLine());
+// int n = Convert.ToInt32(Console.ReadLine());
+// int[,] matrix = FillMatrix(m, n);
+// PrintMatrix(matrix);
+// Console.WriteLine();
+
+// int[] countArray = new int[10];
+
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+// for (int j = 0; j < matrix.GetLength(1); j++)
+// {
+// int index = matrix[i,j];
+// countArray[index]++;
+// }
+// }
+
+// for(int i = 0; i < countArray.Length; i++)
+// {
+// if(countArray[i] != 0)
+// {
+// Console.WriteLine($"Кол-во повторений для {i} = {countArray[i]} раз");
+// }
+// }
---
+++ dz004/Program.cs
@@ -0,0 +1,274 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] FillMatrix(int rows, int columns)
+{
+    int[,] matrix = new int [rows, columns];
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+            {
+            matrix[i,j] = new Random().Next(0,10);
+            }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+Console.WriteLine("Entar rows length: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Entar columns length: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int [,] matrix = FillMatrix(m,n);
+PrintMatrix(matrix);
+Console.WriteLine();
+
+void SortMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+            {
+                for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+                {
+                    if (matrix[i, k] < matrix[i, k + 1])
+                        {
+                            int temp = matrix[i, k + 1];
+                            matrix[i, k + 1] = matrix[i, k];
+                            matrix[i, k] = temp;
+                        }
+                }
+            }
+    }
+}
+
+SortMatrix(matrix);
+PrintMatrix(matrix);
+
+// Задача 56: Задайте прямоугольный двумерный массив.
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке 
+// и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[,] FillMatrix(int rows, int columns)
+{
+    int[,] matrix = new int [rows, columns];
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+            {
+            matrix[i,j] = new Random().Next(0,10);
+            }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+Console.WriteLine("Entar rows length: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Entar columns length: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int [,] matrix = FillMatrix(m,n);
+PrintMatrix(matrix);
+Console.WriteLine();
+RowMinSum(matrix);
+void RowMinSum(int[,] matrix)
+{
+    int min = 0;
+    int minSum = 0;
+    int sum = 0;
+    for (int i = 0; i < matrix.GetLength(1); i++)
+        {
+            min += matrix[0, i];
+        }
+    for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++) sum += matrix[i, j];
+            if (sum < min)
+                {
+                    min = sum;
+                    minSum = i;
+                }
+        sum = 0;
+        }
+    Console.Write($"{minSum + 1} row");
+}
+
+// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+int[,] FillMatrix(int rows, int columns)
+{
+    int[,] matrix = new int [rows, columns];
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+            {
+            matrix[i,j] = new Random().Next(0,10);
+            }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+Console.WriteLine("Entar rows length to matrix: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Entar columns length to matrix: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int [,] matrix = FillMatrix(m,n);
+PrintMatrix(matrix);
+Console.WriteLine();
+int [,] secondMatrix = FillMatrix(m,n);
+PrintMatrix(secondMatrix);
+Console.WriteLine();
+int[,] resultMatrix = new int[m,n];
+
+void SumMatrix(int[,] martrix, int[,] secomdMartrix, int[,] resultMatrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < martrix.GetLength(1); k++)
+      {
+        sum += martrix[i,k] * secomdMartrix[k,j];
+      }
+      resultMatrix[i,j] = sum;
+    }
+  }
+}
+SumMatrix(matrix, secondMatrix, resultMatrix);
+PrintMatrix(resultMatrix);
+
+// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+
+int[,,] FillMatrix(int rows, int columns, int size)
+{
+    int[,,] matrix = new int [rows, columns, size];
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+            {
+            for(int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[i,j,k] = new Random().Next(0,10);
+            }
+
+            }
+    }
+    return matrix;
+}
+
+Console.WriteLine("Entar x length: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Entar y length: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Entar z length: ");
+int p = Convert.ToInt32(Console.ReadLine());
+int [,,] matrix = FillMatrix(m,n,p);
+for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.WriteLine();
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.Write($"{matrix[i, j, k]}({i},{j},{k}) ");
+            }
+        }
+    }
+
+// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+Console.WriteLine("Enter matrix sizq ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, n];
+
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= matrix.GetLength(0) * matrix.GetLength(1))
+{
+  matrix[i, j] = temp;
+  temp++;
+  if (i <= j + 1 && i + j < matrix.GetLength(1) - 1)
+    j++;
+  else if (i < j && i + j >= matrix.GetLength(0) - 1)
+    i++;
+  else if (i >= j && i + j > matrix.GetLength(1) - 1)
+    j--;
+  else
+    i--;
+}
+void PrintNatrix (int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      Console.Write($" {matrix[i,j]} ");
+    }
+    Console.WriteLine();
+  }
+}
+PrintNatrix(matrix);
